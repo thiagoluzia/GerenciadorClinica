d6a94ee726877145140680277c6460e12c73b4ce
thiagomoura <thiago.mouraluzia@gmail.com>
2024-03-05 01:15:22 -0300

    Criação das entidades de domínio, e aplicando as dependências dos projetos.

---
+++ GerenciadorClinica/GC.Core/Entityes/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace GC.Core.Entityes
+{
+    /// <summary>
+    /// Classe base para entidades do sistema, contendo informações básicas compartilhadas entre diferentes tipos de entidades.
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        public int Id { get; private set; }
+    }
+}
